3edaaeefa52cd422d4c3e451634026fafafd0736
Jesus95santana <91024598+Jesus95santana@users.noreply.github.com>
2022-01-20 18:50:08 -0500

    completed react routing... finishing pages to link

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route };
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { Home } from './pages/Home/Home.js';
+import { About } from './pages/About/About.js';
 
 class Index extends React.Component{
   render() {
     return(
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/home' element=<Home /> />
+          <Route path='/about' element=<About /> />
+        </Routes>
+      </Router>
     );
   }
 }
